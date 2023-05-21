34cbf57758ca858276af6a79e1b0e45a254d02a7
sarathrkrishnan <sarath1995rk@gmail.com>
2023-05-21 15:22:15 +0530

    iniotial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/features/landing/screens/landing_screen.dart';
 import 'package:whatsapp_clone/firebase_options.dart';
+import 'package:whatsapp_clone/router.dart';
 import 'package:whatsapp_clone/screens/mobile_layout_screen.dart';
 import 'package:whatsapp_clone/screens/web_layout_screen.dart';
 import 'package:whatsapp_clone/utils/responsive_layout.dart';
@@ -23,12 +25,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
+     
       title: 'Whatsapp UI',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
+         appBarTheme: const AppBarTheme(
+          color: appBarColor,
+        ),
       ),
+       onGenerateRoute: (settings) => generateRoute(settings),
       home: const ResponsiveLayout(
-        mobileScreenLayout: MobileLayoutScreen(),
+        mobileScreenLayout: LandingScreen(),
         webScreenLayout: WebLayoutScreen(),
       ),
     );
